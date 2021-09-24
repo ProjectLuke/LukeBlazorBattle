c0a96aa04fad071912b6921fad5364232d435fac
Luke Ugalino <39897299+LumpiaNation@users.noreply.github.com>
2021-09-23 22:20:31 -0700

    Add project files.

---
+++ LukeBlazorBattle/Client/Pages/Counter.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LukeBlazorBattle.Client.Pages
+{
+    public partial class Counter : ComponentBase
+    {
+    }
+}
